b87b985585cb885f2cd0c240bb670d93986ed862
Superkooka <aymeric.gueracague@gmail.com>
2023-09-05 14:35:28 +0200

    drawing animation line
    Signed-off-by: Superkooka <aymeric.gueracague@gmail.com>

--- src/game/line.ts
+++ src/game/line.ts
@@ -8,6 +8,8 @@ export class Line extends Object2D {
     width: number
     color: string
 
+    animationProgresss: number = 0
+
     constructor (from: vec2, to: vec2, width: number, color: string) {
         super()
 
@@ -18,11 +20,26 @@ export class Line extends Object2D {
     }
 
     redraw(context: CanvasRenderingContext2D): void {
-        context.beginPath();
-        context.moveTo(this.from.x, this.from.y);
-        context.lineTo(this.to.x, this.to.y);
-        context.lineWidth = this.width;
-        context.strokeStyle = this.color;
-        context.stroke();
+        this.animationProgresss += 0.02 //TODO: Do not based animation progress on refresh, higher framerate, faster animation
+
+        if (this.animationProgresss <= 1) {
+            const progressedTo = new vec2(
+                this.from.x + (this.to.x - this.from.x) * this.animationProgresss,
+                this.from.y + (this.to.y - this.from.y) * this.animationProgresss,
+            )
+
+            traceLine(this.from, progressedTo, this.width, this.color, context)
+        } else {
+            traceLine(this.from, this.to, this.width, this.color, context)
+        }
     }
+}
+
+function traceLine(from: vec2, to: vec2, width: number, color: string, context: CanvasRenderingContext2D) {
+    context.beginPath();
+    context.moveTo(from.x, from.y);
+    context.lineTo(to.x, to.y);
+    context.lineWidth = width;
+    context.strokeStyle = color;
+    context.stroke();
 }
