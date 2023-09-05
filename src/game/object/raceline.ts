d3d0b338a1a3fec6c51b626858c9a129872dc749
Superkooka <aymeric.gueracague@gmail.com>
2023-09-05 16:51:15 +0200

    move animation to function
    Signed-off-by: Superkooka <aymeric.gueracague@gmail.com>

--- src/game/object/raceline.ts
+++ src/game/object/raceline.ts
@@ -4,20 +4,22 @@ import { vec2 } from '../../engine/vec2.js'
 export class RaceLine extends Line {
     animationProgresss: number = 0
 
+    writeLineAnimation(context: CanvasRenderingContext2D): void {
+        const progressedTo = new vec2(
+            this.from.x + (this.to.x - this.from.x) * this.animationProgresss,
+            this.from.y + (this.to.y - this.from.y) * this.animationProgresss,
+        )
+
+        traceLine(this.from, progressedTo, this.width, this.color, context)
+    }
+
     redraw(context: CanvasRenderingContext2D): void {
-        this.animationProgresss += 0.02 //TODO: Do not based animation progress on refresh, higher framerate, faster animation
-
         if (this.animationProgresss <= 1) {
-            const progressedTo = new vec2(
-                this.from.x + (this.to.x - this.from.x) * this.animationProgresss,
-                this.from.y + (this.to.y - this.from.y) * this.animationProgresss,
-            )
-
-            traceLine(this.from, progressedTo, this.width, this.color, context)
+            this.animationProgresss += 0.02 //TODO: Do not based animation progress on refresh, higher framerate, faster animation
+            this.writeLineAnimation(context)
         } else {
             traceLine(this.from, this.to, this.width, this.color, context)
         }
-
     }
 }
 
